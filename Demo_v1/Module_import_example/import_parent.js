729358beb4dc4c1785f62648afabe14c1f8dd5d9
Gattupalli Tejaswi <gsteja1234@gmail.com>
2019-03-23 15:38:46 +0530

    Create import_parent.js

---
+++ Demo_v1/Module_import_example/import_parent.js
@@ -0,0 +1,15 @@
+module.exports = {
+    A : class
+    {
+        constructor()
+        {
+            this.a=1
+            this.b=2
+        }
+        printDets()
+        {
+            console.log(this.a)
+            console.log(this.b) 
+        }
+    }
+}
