7edf6757c6504e294af54cf7f9f598672f215af3
Gattupalli Tejaswi <gsteja1234@gmail.com>
2019-03-27 18:14:18 +0530

    Create user_node.js

---
+++ Tejaswi/MinerUser_v3/user_node.js
@@ -0,0 +1,79 @@
+const crypto = require('crypto')
+const Swarm = require('discovery-swarm')
+const defaults = require('dat-swarm-defaults')
+const getPort = require('get-port')
+const readline = require('readline');
+
+// Message types-
+// 10 : User message with transaction to be mined
+
+class UserNode
+{
+  constructor()
+  {
+    //List of this.peers: {peer id, connection}
+    this.peers = {}
+    this.connSeq = 0
+    this.myId = crypto.randomBytes(32)
+    console.log('Your identity: ' + this.myId.toString('hex'))  
+    this.config = defaults({id: this.myId,})
+    this.sw = Swarm(this.config)
+    this.ConnectionAwait()
+  }
+  
+  BroadcastQuery(tx) {
+    for (let id in this.peers) {
+      this.peers[id].conn.write(
+        JSON.stringify(
+          {
+            "type":10,
+            "content":tx
+          }
+        )
+      )
+    }
+  }
+
+  
+async ConnectionAwait() {
+  this.port = await getPort()
+  this.sw.listen(this.port)
+  // console.log('Listening to port: ' + port)
+  this.sw.join('miner_channel')
+  this.sw.on('connection', (conn, info) => {
+    const seq = this.connSeq
+    const peerId = info.id.toString('hex')
+    // conn.on('data', data => {
+    //   console.log('Received Message ' +data.toString())
+    // })
+    console.log(`Connected #${seq} to peer: ${peerId}`)
+    conn.on('close', () => {
+      console.log(`Connection ${seq} closed, peer id: ${peerId}`)
+      if (this.peers[peerId].seq === seq) {
+        delete this.peers[peerId]
+      }
+    })
+    if (!this.peers[peerId]) {
+      this.peers[peerId] = {}
+    }
+    this.peers[peerId].conn = conn
+    this.peers[peerId].seq = seq
+    this.connSeq++
+  })
+} 
+
+
+}
+
+// Sample Usage:
+let usn = new UserNode()
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  // console.log(`Received: ${input}`);
+  usn.BroadcastQuery(input);
+});
